a9bc93505cae499e7e1c2bcdb0971f9b67256121
김정호 <kjungho@onycom.com>
2024-05-08 11:26:10 +0900

    동일서버내 실행중인 분석프로그램이 제한없이 API에 접근할 수 있게 수정 (127.0.0.1 IP로 접근하는 요청은 인증키 검증 필요없음)

--- src/main/java/org/ankus/api/auth/ServiceKeyAuthIntercepter.java
+++ src/main/java/org/ankus/api/auth/ServiceKeyAuthIntercepter.java
@@ -1,5 +1,6 @@
 package org.ankus.api.auth;
 
+import lombok.extern.slf4j.Slf4j;
 import org.ankus.model.ServiceKeyInfo;
 import org.ankus.service.ServiceKeyInfoSvc;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +11,14 @@ import org.springframework.web.servlet.handler.WebRequestHandlerInterceptorAdapt
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.file.AccessDeniedException;
 
 /**
  * API 인증을 위한 인터셉터
  */
+@Slf4j
 @Component
 public class ServiceKeyAuthIntercepter extends WebRequestHandlerInterceptorAdapter {
 
@@ -37,16 +41,50 @@ public class ServiceKeyAuthIntercepter extends WebRequestHandlerInterceptorAdapt
 
         // API 관련 URL 주소에 대한 서비스키를 확인 및 인증
         if (request.getRequestURI().indexOf("/api") == 0){
-            String serviceKey = request.getParameter("serviceKey");
-            if (serviceKey == null){
-                throw new AccessDeniedException("The serviceKey parameter does not exist.");
-            }else if (!serviceKeyInfoSvc.isValidServiceKey(serviceKey)){
-                throw new AccessDeniedException("The serviceKey parameter is not authenticated.");
+
+
+            String remoteIp = request.getHeader("X-Forwarded-For");
+            if (remoteIp == null) {
+                remoteIp = request.getHeader("Proxy-Client-IP");
+            }
+            if (remoteIp == null) {
+                remoteIp = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (remoteIp == null) {
+                remoteIp = request.getHeader("HTTP_CLIENT_IP");
+            }
+            if (remoteIp == null) {
+                remoteIp = request.getHeader("HTTP_X_FORWARDED_FOR");
+            }
+            if (remoteIp == null) {
+                remoteIp = request.getRemoteAddr();
+            }
+            //  현재 시스템의 IP 가져오기
+            InetAddress local = null;
+            try {
+                local = InetAddress.getLocalHost();
+            }catch (UnknownHostException e ) {
+                e.printStackTrace();
+            }
+            String localIp = "";
+            if( local != null ){
+                localIp = local.getHostAddress();
             }
 
+            // 아무나 API 접근 못하도록 접근 제한
+            if ("127.0.0.1".equals(remoteIp.trim()) || "0:0:0:0:0:0:0:1".equals(remoteIp.trim()) || localIp.equals(remoteIp.trim())){
+                // 로컬IP 확인을 통한 접근 제한(즉, 동일서버내에 구동되는 프로그램만 무제한 접근 가능)
+                log.info("동일서버내 API 접근(요청 URL: "+request.getRequestURL()+")");
+            }else{
+                // 서비스 key 인증으로 접근제한
+                String serviceKey = request.getParameter("serviceKey");
+                if (serviceKey == null){
+                    throw new AccessDeniedException("The serviceKey parameter does not exist.");
+                }else if (!serviceKeyInfoSvc.isValidServiceKey(serviceKey)){
+                    throw new AccessDeniedException("The serviceKey parameter is not authenticated.");
+                }
+            }
         }
-
-
         return super.preHandle(request, response, handler);
     }
 }
